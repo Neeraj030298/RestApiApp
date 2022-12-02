8b3a14409251b0cb244ffa868ee42d820732d1a3
Neeraj <neginee01raj@gmil.com>
2022-12-02 19:34:40 +0530

    Updated 2

--- src/main/java/com/example/RestApi/MyController.java
+++ src/main/java/com/example/RestApi/MyController.java
@@ -1,7 +1,5 @@
 package com.example.RestApi;
 
-//import org.apache.catalina.webresources.StandardRoot;
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
